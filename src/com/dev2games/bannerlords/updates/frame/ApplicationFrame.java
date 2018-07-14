8c60920018d93bbc1f90c39070ab8bd724644300
Nelson Sanchez <nelson@dev2games.com>
2018-07-14 03:50:32 -0400

    ApplicationFrame and Sound class

---
+++ src/com/dev2games/bannerlords/updates/frame/ApplicationFrame.java
@@ -0,0 +1,9 @@
+package com.dev2games.bannerlords.updates.frame;
+
+/**
+ * <p>Handles loading the main GUI for our application</p>
+ * @author Nelson Sanchez
+ */
+
+//TODO add a JFrame to the application
+public class ApplicationFrame { }
---
+++ src/com/dev2games/bannerlords/updates/sound/Sound.java
@@ -0,0 +1,4 @@
+package com.dev2games.bannerlords.updates.sound;
+
+//TODO add a way to manage sounds / notifications for when a new update occurs
+public class Sound { }
