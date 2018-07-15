516de95142987f576de49aadc2c04b302a7f4a41
Nelson <nelson@dev2games.com>
2018-07-14 21:02:49 -0400

    Start of ApplicationFrame

--- src/com/dev2games/bannerlords/updates/frame/ApplicationFrame.java
+++ src/com/dev2games/bannerlords/updates/frame/ApplicationFrame.java
@@ -1,9 +1,23 @@
 package com.dev2games.bannerlords.updates.frame;
 
+import javax.swing.JFrame;
+
 /**
  * <p>Handles loading the main GUI for our application</p>
  * @author Nelson Sanchez
  */
 
 //TODO add a JFrame to the application
-public class ApplicationFrame { }
+public class ApplicationFrame {
+	
+	JFrame frame;
+	
+	public ApplicationFrame() {
+		//Initializes the JFrame
+		frame = new JFrame("Taleworlds Forum Updates");
+		//Makes it so the application closes on click
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+}
